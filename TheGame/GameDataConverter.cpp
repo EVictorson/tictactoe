4bf2e9d4dbda847740213929abacd59f6e1ce656
Tessa Power <tessa@tessapower.co>
2020-05-08 19:48:03 +1200

    Expand function that converts game state data into display data

--- TheGame/GameDataConverter.cpp
+++ TheGame/GameDataConverter.cpp
@@ -1,11 +1,43 @@
 #include "GameDataConverter.h"
 
-//std::string convertGameStateToVisualRepresentation(char gameState[])
-//{
-//	gameBoard = R"(                                     |   |   \n                                    )" + std::to_string(1) + R"( |  )" + std::to_string(2) + R"( |  )" + std::to_string(3) + R"( \n                                  ___|___|___\n                                     |   |   \n                                   )" + std::to_string(4) + R"( | )" + std::to_string(5) + R"( | )" + std::to_string(6) + R"( \n                                  ___|___|___\n                                     |   |   \n                                   )" + std::to_string(7) + R"( | " + std::to_string(8) + R"( | )" + std::to_string(9) + R"( \n                                     |   |   \n\n)";
-//
-//	return (gameBoard);
-//}
+std::string GameDataConverter::convertGameStateToVisualRepresentation(char gameState[])
+{
+	// Access the value of the array element instead of the ASCII number of the value
+
+	char *pos1 = &gameState[0];
+
+	/*
+	The string below will be populated either with placeholder numbers if
+	no moves have been made or the symbol of a player who has made a move,
+	e.g.
+
+	       Empty:     |   |                   Moves     |   |
+	                1 | 2 | 3                 Made:   1 | X | 3
+	               ___|___|___                       ___|___|___
+	                  |   |                             |   |
+	                4 | 5 | 6                         4 | 5 | O
+	               ___|___|___                       ___|___|___
+	                  |   |                             |   |
+	                7 | 8 | 9                         X | 8 | 9
+	                  |   |                             |   |
+	*/
+
+
+
+	std::string gameBoard = R"(
+	                                     |   |
+	                                   )" + std::to_string(gameState[0]) + " | " + std::to_string(gameState[1]) + " | " + std::to_string(gameState[2]) + " \n"
+		+ R"(	                                  ___|___|___
+	                                     |   |
+	                                   )" + std::to_string(gameState[3]) + " | " + std::to_string(gameState[4]) + " | " + std::to_string(gameState[5]) + " \n"
+		+ R"(	                                  ___|___|___
+	                                     |   |
+	                                   )" + std::to_string(gameState[6]) + " | " + std::to_string(gameState[7]) + " | " + std::to_string(gameState[8]) + " \n"
+		+ R"(	                                     |   |
+		)";
+
+	return (gameBoard);
+}
 
 //std::string convertScoreToVisualRepresentation(int playerOneScore, int playerTwoScore)
 //{
