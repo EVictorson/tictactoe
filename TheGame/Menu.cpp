8cbecb23f4832b2d2ff3aaee0ab622e2af4b5a9d
Tessa Power <tessa@tessapower.co>
2020-05-20 11:50:52 +1200

    Add shell of functions for Menu class

---
+++ TheGame/Menu.cpp
@@ -0,0 +1,21 @@
+#include "Menu.h"
+
+int Menu::getUserInput()
+{
+
+}
+
+int Menu::checkForAnyKeyPressed(int userInput)
+{
+
+}
+
+void Menu::navigateToSubmenu(int userInput)
+{
+
+}
+
+void Menu::returnToMainMenu(int userInput)
+{
+
+}
