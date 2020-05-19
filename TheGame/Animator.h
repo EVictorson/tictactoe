5f9632731650c35f7b499721a5366e66d67751ec
Tessa Power <tessa@tessapower.co>
2020-05-19 17:07:36 +1200

    Add headers and function to Animator header file

---
+++ TheGame/Animator.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifdef _WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#include <stdlib.h>
+#include <array>
+#include <conio.h>
+#include <iostream>
+#include <string>
+
+class Animator
+{
+public:
+	void introduceGame();
+	
+};
+
