8c89bacef11a7784de1136a15cfcd5d1bdf0641b
Tessa Power <tessa@tessapower.co>
2020-05-19 18:00:10 +1200

    Make animate function more general

--- TheGame/Animator.cpp
+++ TheGame/Animator.cpp
@@ -1,65 +1,19 @@
 #include "Animator.h"
 
-void Animator::introduceGame()
+void Animator::animate(std::array <std::string, 5> animationSequence)
 {
-	std::string step1 = R"(
-Let's play...
-   |   |
-   |   |
-___|___|___
-   |   |
-   |   |
-___|___|___
-   |   |
-   |   |
-   |   |
-)";
-
-	std::string step2 = R"(
-Let's play...
-   |   |
-   |   |
-___|___|___
-   |   |
-TIC|   |
-___|___|___
-   |   |
-   |   |
-   |   |
-)";
-
-	std::string step3 = R"(
-Let's play...
-   |   |
-   |   |
-___|___|___
-   |   |
-TIC|TAC|
-___|___|___
-   |   |
-   |   |
-   |   |
-)";
-
-	std::string step4 = R"(
-Let's play...
-   |   |
-   |   |
-___|___|___
-   |   |
-TIC|TAC|TOE!
-___|___|___
-   |   |
-   |   |
-   |   |
-)";
-
-    std::array <std::string, 4> sequence { step1, step2, step3, step4};
-
-    for (auto step : sequence)
+    for (auto step : animationSequence)
     {
-        std::cout << step << std::endl;
-        Sleep(1000);
-        system("CLS");
+        if (step != "\0")
+        {
+            system("CLS");
+            std::cout << step << std::endl;
+            Sleep(1000);
+        }
+        else
+        {
+            break;
+        }
     }
 }
+
