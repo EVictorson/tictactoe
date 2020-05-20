26a7ea7cdc20f7404b1c84ca3d1a5d7ce70e205a
Tessa Power <tessa@tessapower.co>
2020-05-20 21:21:20 +1200

    Update function that waits for user to press any key

--- TheGame/Menu.h
+++ TheGame/Menu.h
@@ -5,11 +5,14 @@
 #include <conio.h>
 #include <iostream>
 #include <string>
+#include "menuDisplayer.h"
 
 class Menu
 {
+public:
 	int getUserInput();
-	bool checkForAnyKeyPressed(int userInput);
+	bool validateUserInput(int userInput, std::array <int, 3> submenuOptions);
+	bool checkForAnyKeyPressed();
 	void navigateToSubmenu(int userInput);
 	void returnToMainMenu(bool anyKeyPressed);
 	std::array <int, 3> submenuOptions{ 1, 2, 3 };
