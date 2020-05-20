3c8b315b8b64e3772a6e98f8e6159c1e56aeeb4d
Tessa Power <tessa@tessapower.co>
2020-05-20 16:17:47 +1200

    Expand functions to navigate menu

--- TheGame/Menu.cpp
+++ TheGame/Menu.cpp
@@ -29,10 +29,31 @@ bool Menu::checkForAnyKeyPressed(int userInput)
 
 void Menu::navigateToSubmenu(int userInput)
 {
+	validUserInput = false;
+	userInput = _getch();
 
+	while (!validUserInput)
+	{
+		if (userInput == '1')
+		{
+			validUserInput = true;
+			printInstructions();
+		}
+		else if (userInput == '2')
+		{
+			validUserInput = true;
+			showScoreboard();
+		}
+		else if (userInput == '3')
+		{
+			validUserInput = true;
+			showCredits();
+		}
+
+	}
 }
 
-void Menu::returnToMainMenu(int userInput)
+void Menu::returnToMainMenu(bool anyKeyPressed)
 {
 
 }
